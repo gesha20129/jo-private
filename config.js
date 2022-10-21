f4bf980133b38813a3bef15db84932b827151219
gesha20129 <91309083+gesha20129@users.noreply.github.com>
2022-10-22 00:21:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Anu BOTZ'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6285891318105','6285891318105','6285891318105']
+global.premium = ['6285891318105']
+global.packname = 'Anu BOTZ'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
